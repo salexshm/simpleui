5c94fa41e8fd617fbf610243baa3d7934a68324c
@aleksandr_starikov <salex.shm@mail.ru>
2024-08-18 16:43:09 +0600

    handlers.py change def sample1_on_input

--- handlers.py
+++ handlers.py
@@ -1,9 +1,12 @@
 def sample1_on_create(hashMap, _files=None, _data=None):
-
+    if not hashMap.containsKey("a"):
+        hashMap.put("a", "")
+    if not hashMap.containsKey("b"):
+        hashMap.put("b", "")
     return hashMap
 
 
 def sample1_on_input(hashMap, _files=None, _data=None):
     if hashMap.get("listener") == "btn_res":
-        hashMap.put("toast", str(int(hashMap.get("a")) + int(hashMap.get("b"))))
+        hashMap.put("toast", str(hashMap.get("a") + hashMap.get("b")))
     return hashMap
