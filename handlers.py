efca9e6c8ce8e995ed4341421442c3f2520fc6c4
salexshm <68847026+salexshm@users.noreply.github.com>
2024-08-18 16:23:39 +0600

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,5 +1,8 @@
 def sample1_on_create(hashMap, _files=None, _data=None):
-
+    if not hashMap.containsKey("a"):
+        hashMap.put("a", "")
+    if not hashMap.containsKey("b"):
+        hashMap.put("b", "")
     return hashMap
 
 
