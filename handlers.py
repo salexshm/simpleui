068fd7e152adf72043f19cf072bf6ab6599b1a7a
@aleksandr_starikov <salex.shm@mail.ru>
2024-08-18 16:17:05 +0600

    handlers.py change def sample1_on_create

--- handlers.py
+++ handlers.py
@@ -1,8 +1,5 @@
 def sample1_on_create(hashMap, _files=None, _data=None):
-    if not hashMap.containsKey("a"):
-        hashMap.put("a", "")
-    if not hashMap.containsKey("b"):
-        hashMap.put("b", "")
+
     return hashMap
 
 
