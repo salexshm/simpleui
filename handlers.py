f1ef7c7c38ead9ce57c93a310e07835a7e47c5cc
salexshm <salex.shm@gmail.com>
2024-10-04 14:37:06 +0500

    create func sample1_on_create

--- handlers.py
+++ handlers.py
@@ -3,3 +3,10 @@ def sample1_on_input(hashMap,_files=None,_data=None):
       hashMap.put("toast",str(int(hashMap.get("a"))+int(hashMap.get("b"))))
 
   return hashMap
+
+def sample1_on_create(hashMap,_files=None,_data=None):
+  if not hashMap.containsKey("a"):
+      hashMap.put("a","")
+  if not hashMap.containsKey("b"):
+      hashMap.put("b","")
+  return hashMap
