648782f196bf10d4b6ae972c0c753294cfee2c8e
salexshm <salex.shm@gmail.com>
2024-10-04 14:23:51 +0500

    create func sample1_on_input

--- handlers.py
+++ handlers.py
@@ -1,8 +1,5 @@
-def sample1_on_create(hashMap, _files=None, _data=None):
+def sample1_on_input(hashMap,_files=None,_data=None):
+  if hashMap.get("listener")=="btn_res":
+      hashMap.put("toast",str(int(hashMap.get("a"))+int(hashMap.get("b"))))
 
-    return hashMap
-
-
-def sample1_on_input(hashMap, _files=None, _data=None):
-
-    return hashMap
+  return hashMap
