4a21b0e87ccd5c32930d0a5c9a54cb7dbd25f1e3
salexshm <salex.shm@gmail.com>
2024-10-09 16:13:20 +0500

    upgrade func sample1_on_input and sample1_on_create

--- handlers.py
+++ handlers.py
@@ -1,12 +1,12 @@
 def sample1_on_input(hashMap,_files=None,_data=None):
-  if hashMap.get("listener")=="btn_res":
-      hashMap.put("toast",str(int(hashMap.get("a"))+int(hashMap.get("b"))))
+    if hashMap.get("listener")=="btn_res":
+        hashMap.put("toast",str(int(hashMap.get("a"))+int(hashMap.get("b"))))
+    return hashMap
 
-  return hashMap
 
 def sample1_on_create(hashMap,_files=None,_data=None):
-  if not hashMap.containsKey("a"):
-      hashMap.put("a","")
-  if not hashMap.containsKey("b"):
-      hashMap.put("b","")
-  return hashMap
+    if not hashMap.containsKey("a"):
+        hashMap.put("a","")
+    if not hashMap.containsKey("b"):
+        hashMap.put("b","")
+    return hashMap
