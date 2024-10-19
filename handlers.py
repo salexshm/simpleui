f59db606d9e961889867594e4ef65ccd8a3d888f
salexshm <salex.shm@gmail.com>
2024-10-19 14:47:20 +0500

    create func init_on_start

--- handlers.py
+++ handlers.py
@@ -1,3 +1,5 @@
+import json
+
 def sample1_on_input(hashMap,_files=None,_data=None):
     if hashMap.get("listener")=="btn_res":
         hashMap.put("toast",str(int(hashMap.get("a"))+int(hashMap.get("b"))))
@@ -11,6 +13,26 @@ def sample1_on_create(hashMap,_files=None,_data=None):
         hashMap.put("b","")
     return hashMap
 
-def print_barcode(hashMap,_files=None,_data=None):
-    hashMap.put("toast", hashMap.get("barcode"))
-    return hashMap
+
+def barcode_on_input(hashMap,_files=None,_data=None):
+    # handlers code
+    if hashMap.get("listener") == 'barcode':  # check scan event
+        b = hashMap.get('barcode')  # barcode variable
+        hashMap.put("toast", hashMap.get(b))
+        return hashMap
+
+
+def init_on_start(hashMap,_files=None,_data=None):
+   hashMap.put("SQLConnectDatabase","test1.DB")
+   hashMap.put("SQLExec",json.dumps(
+       {
+           "query":
+               "create table IF NOT EXISTS Record(id integer primary key autoincrement,art text, barcode text, name text, qty real)",
+           "params":
+               ""
+       }
+   )
+               )
+
+
+   return hashMap
